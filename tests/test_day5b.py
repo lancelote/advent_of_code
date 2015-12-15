d76ac59871b9f370c24b5383b445ded34d37a02c
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-12-15 16:24:32 +0300

    Day 5 Part 2

---
+++ tests/test_day5b.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+# pylint: disable=missing-docstring
+
+"""
+Day 5 Puzzle Part 2 tests
+"""
+
+import unittest
+
+from src.day5b import is_nice
+
+
+class TestIsNice(unittest.TestCase):
+
+    def test_returns_correct_result(self):
+        self.assertTrue(is_nice('qjhvhtzxzqqjkmpb'))
+        self.assertTrue(is_nice('xxyxx'))
+        self.assertFalse(is_nice('uurcxstgmygtbstg'))
+        self.assertFalse(is_nice('ieodomkazucvgmuy'))
+        self.assertFalse(is_nice('aaa'))
