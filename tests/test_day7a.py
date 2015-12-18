95f5033b17591ddb1501f3f2fa50825c4298ac0f
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-12-18 15:59:58 +0300

    Template for day 7

---
+++ tests/test_day7a.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# pylint: disable=missing-docstring
+
+"""
+Day 7 Puzzle Part 1 tests
+"""
+
+import unittest
+
+from src.day6a import solve
+
+
+class TestSolve(unittest.TestCase):
+
+    def test_returns_correct_result(self):
+        self.assertTrue(True)
