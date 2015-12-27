3a266618d52c7ed066375140605c74fa798a18ba
Pavel Karateev <karateev.pavel@ya.ru>
2015-12-27 20:32:02 +0300

    Work in progress Day 7 Part 1

--- src/day7a.py
+++ src/day7a.py
@@ -63,7 +63,7 @@ what signal is ultimately provided to wire a?
 """
 
 import re
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 
 PATTERN = re.compile(r'^(?:(?:(\w*) |)([A-Z]*) |)(\w*) -> (\w*)$')
 
@@ -73,14 +73,42 @@ def process_data(data):
     command = namedtuple('Command', ['input_a', 'gate', 'input_b', 'output'])
 
     for line in data.strip().split('\n'):
-        input_a, gate, input_b, output = re.match(PATTERN, line).groups()
-        processed_data.append(command(input_a, gate, input_b, output))
+        a, gate, b, output = re.match(PATTERN, line).groups()
+        b = int(b) if gate in (None, 'LSHIFT', 'RSHIFT') else b
+        processed_data.append(command(a, gate, b, output))
 
     return processed_data
 
 
+class Signal(object):
+
+    def __init__(self, a=None, gate=None, b=None, output=None):
+        self.a = a
+        self.gate = gate
+        self.b = b
+        self.output = output
+
+    def get_value(self, signals):
+        __value = None
+
+        if self.gate is None:
+            __value = self.b
+        elif self.gate == 'AND':
+            __value = signals[self.a].get_value(signals) & signals[self.b].get_value(signals)
+        elif self.gate == 'OR':
+            __value = signals[self.a].get_value(signals) | signals[self.b].get_value(signals)
+        elif self.gate == 'LSHIFT':
+            __value = signals[self.a].get_value(signals) << self.b
+        elif self.gate == 'RSHIFT':
+            __value = signals[self.a].get_value(signals) >> self.b
+        elif self.gate == 'NOT':
+            __value = 65535 - signals[self.b].get_value(signals)
+        return __value
+
+
 def solve(task):
+    signals = dict()
     commands = process_data(task)
     for command in commands:
-        pass
-    return task
+        signals[command.output] = Signal(*command)
+    return signals['a'].get_value(signals)
