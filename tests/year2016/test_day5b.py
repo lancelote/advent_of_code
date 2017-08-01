9696e32c6b739a8763c1be3f6b8c3321b7d924d9
Pavel Karateev <karateev.pavel@ya.ru>
2017-08-01 10:18:24 +0300

    Fixes #79 - mocks out print for tests

--- tests/year2016/test_day5b.py
+++ tests/year2016/test_day5b.py
@@ -1,6 +1,7 @@
 """2016 - Day 5 Part 2: How About a Nice Game of Chess tests."""
 
 import unittest
+from unittest import mock
 
 from src.year2016.day5b import solve
 
@@ -8,4 +9,5 @@ from src.year2016.day5b import solve
 class SolveTest(unittest.TestCase):
 
     def test_solve(self):
-        self.assertEqual(solve('abc'), '05ace8e3')
+        with mock.patch('src.year2016.day5b.print_password'):
+            self.assertEqual(solve('abc'), '05ace8e3')
