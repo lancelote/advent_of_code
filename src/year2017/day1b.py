3641cac9590a602526f23bb3d43af9d0dd2cfd0e
Pavel Karateev <karateev.pavel@ya.ru>
2017-12-01 22:01:29 +0300

    Fixes #83 - 2017 Day 1 Part 2 Solution

---
+++ src/year2017/day1b.py
@@ -0,0 +1,35 @@
+"""2017 - Day 1 Part 2: Inverse Captcha.
+
+You notice a progress bar that jumps to 50% completion. Apparently, the door
+isn't yet satisfied, but it did emit a star as encouragement. The instructions
+change:
+
+Now, instead of considering the next digit, it wants you to consider the digit
+halfway around the circular list. That is, if your list contains 10 items, only
+include a digit in your sum if the digit 10/2 = 5 steps forward matches it.
+Fortunately, your list has an even number of elements.
+
+For example:
+
+    - 1212 produces 6: the list contains 4 items, and all four digits match the
+      digit 2 items ahead.
+    - 1221 produces 0, because every comparison is between a 1 and a 2.
+    - 123425 produces 4, because both 2s match each other, but no other digit
+      has a match.
+    - 123123 produces 12.
+    - 12131415 produces 4.
+
+What is the solution to your new captcha?
+"""
+
+
+def solve(task: str) -> int:
+    """Sum all digits that match the halfway around digit in the list."""
+    result = 0
+    task = task.strip()
+    shift = len(task) // 2
+
+    for i, digit in enumerate(task):
+        if digit == task[(i + shift) % len(task)]:
+            result += int(digit)
+    return result
---
+++ tests/year2017/test_day1b.py
@@ -0,0 +1,21 @@
+# pylint: disable=no-self-use
+
+"""2017 - Day 1 Part 2: Inverse Captcha."""
+
+from src.year2017.day1b import solve
+
+
+class TestSolve:
+
+    def test_all_match(self):
+        assert solve('1212') == 6
+        assert solve('123123') == 12
+
+    def test_no_match(self):
+        assert solve('1221') == 0
+
+    def test_one_match(self):
+        assert solve('123425') == 4
+
+    def test_few_match(self):
+        assert solve('12131415') == 4
