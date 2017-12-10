0ad833702d1b43d26230a8e4351ae8432a90058a
Pavel Karateev <karateev.pavel@ya.ru>
2017-12-10 14:27:29 +0300

    Fixes #89 - 2017 Day 4 Part 2 Solution

---
+++ tests/year2017/test_day4b.py
@@ -0,0 +1,21 @@
+# pylint: disable=no-self-use
+
+"""2017 - Day 4 Part 2: High-Entropy Passphrases tests."""
+
+import pytest
+
+from src.year2017.day4b import anagrams
+
+
+@pytest.mark.parametrize(
+    ('passphrase', 'expected'),
+    [
+        ('abcde fghij', True),
+        ('abcde xyz ecdab', False),
+        ('a ab abc abd abf abj', True),
+        ('iiii oiii ooii oooi oooo', True),
+        ('oiii ioii iioi iiio', False)
+    ]
+)
+def test_anagrams(passphrase, expected):
+    assert anagrams(passphrase) == expected
