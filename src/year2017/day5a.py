b7aa0bfff7d071bdf5fa4e8c13b7196513f5d81f
Pavel Karateev <karateev.pavel@ya.ru>
2017-12-12 21:08:12 +0300

    Simplifies 2017 Day 5 Part 1 solution

--- src/year2017/day5a.py
+++ src/year2017/day5a.py
@@ -48,12 +48,9 @@ def solve(task: str) -> int:
     steps = 0
     data = [int(item) for item in task.strip().split('\n')]
 
-    while current_index >= 0:
-        try:
-            next_index = current_index + data[current_index]
-            data[current_index] += 1
-            current_index = next_index
-            steps += 1
-        except IndexError:
-            break
+    while 0 <= current_index < len(data):
+        next_index = current_index + data[current_index]
+        data[current_index] += 1
+        current_index = next_index
+        steps += 1
     return steps
