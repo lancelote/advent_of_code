7ee081838221b883b3a2b1ad51ec6de6c72b59f0
Pavel Karateev <karateev.pavel@ya.ru>
2017-12-31 13:06:02 +0300

    Fixes #98 - 2017 Day 9 Part 1 Solution

--- src/year2017/day9a.py
+++ src/year2017/day9a.py
@@ -65,4 +65,26 @@ What is the total score for all groups in your input?
 
 
 def solve(task: str) -> int:
-    pass
+    """Count total group score."""
+    score = 0
+    total = 0
+    garbage = False
+    escape = False
+
+    for token in task.strip():
+        if escape:
+            escape = False
+        elif token == '>':
+            garbage = False
+        elif token == '!':
+            escape = True
+        elif garbage:
+            pass
+        elif token == '{':
+            score += 1
+            total += score
+        elif token == '}':
+            score -= 1
+        elif token == '<':
+            garbage = True
+    return total
--- tests/year2017/test_day9a.py
+++ tests/year2017/test_day9a.py
@@ -1,1 +1,22 @@
 """2017 - Day 9 Part 1: Stream Processing."""
+
+import pytest
+
+from src.year2017.day9a import solve
+
+
+@pytest.mark.parametrize(
+    ('stream', 'total_score'),
+    [
+        ('{}', 1),
+        ('{{{}}}', 6),
+        ('{{},{}}', 5),
+        ('{{{},{},{{}}}}', 16),
+        ('{<a>,<a>,<a>,<a>}', 1),
+        ('{{<ab>},{<ab>},{<ab>},{<ab>}}', 9),
+        ('{{<!!>},{<!!>},{<!!>},{<!!>}}', 9),
+        ('{{<a!>},{<a!>},{<a!>},{<ab>}}', 3),
+    ]
+)
+def test_solve(stream, total_score):
+    assert solve(stream) == total_score
