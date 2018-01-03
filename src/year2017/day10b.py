aafab7f2176cde491fcebb3c8802c53bed384285
Pavel Karateev <karateev.pavel@ya.ru>
2018-01-03 16:33:43 +0300

    Fixes #101 - 2017 Day 10 Part 2 Solution

--- src/year2017/day10b.py
+++ src/year2017/day10b.py
@@ -64,6 +64,42 @@ Hash of your puzzle input? Ignore any leading or trailing whitespace you might
 encounter.
 """
 
+from typing import List
+
+from functools import reduce
+from operator import xor
+
+from src.year2017.day10a import Rope
+
+
+def process_data(data: str) -> List[int]:
+    """Convert each character into ASCII byte code."""
+    return [ord(char) for char in data.strip()]
+
+
+def split(sequence: List, chunk) -> List[List]:
+    """Split sequence into equal segments of the given length."""
+    return [sequence[i * chunk:(i + 1) * chunk] for i in range(chunk)]
+
+
+def compress(sparse_hash: List[int], chunk=16) -> List[int]:
+    """Compress sparse hash into dense by XORing each 16 items."""
+    return [reduce(xor, segment) for segment in split(sparse_hash, chunk)]
+
+
+def to_hex(number: int) -> str:
+    """Convert number to hex and made a basic formatting."""
+    return hex(number)[2:].zfill(2)
+
 
 def solve(task: str) -> str:
-    pass
+    """Find Knot Hash of the given input."""
+    rope = Rope()
+    lengths = process_data(task) + [17, 31, 73, 47, 23]
+    for _ in range(64):
+        for length in lengths:
+            rope.reverse(length)
+            rope.move(length)
+    sparse_hash = rope.nodes
+    dense_hash = compress(sparse_hash)
+    return ''.join(to_hex(number) for number in dense_hash)
--- tests/year2017/test_day10b.py
+++ tests/year2017/test_day10b.py
@@ -1,1 +1,49 @@
 """2017 - Day 10 Part 2: Knot Hash tests."""
+
+import pytest
+
+from src.year2017.day10b import compress, process_data, solve, split, to_hex
+
+
+def test_process_data():
+    assert process_data('1,2,3') == [49, 44, 50, 44, 51]
+
+
+@pytest.mark.parametrize(
+    ('sequence', 'chunk', 'expected'),
+    [
+        ([0, 1, 2, 3], 2, [[0, 1], [2, 3]]),
+        ([0, 1, 2, 3, 4, 5, 6, 7, 8], 3, [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+    ]
+)
+def test_split(sequence, chunk, expected):
+    assert split(sequence, chunk) == expected
+
+
+def test_compress():
+    assert compress([0, 1, 2, 3, 4, 5, 6, 7, 8], chunk=3) == [3, 2, 9]
+
+
+@pytest.mark.parametrize(
+    ('number', 'expected'),
+    [
+        (64, '40'),
+        (7, '07'),
+        (255, 'ff')
+    ]
+)
+def test_to_hex(number, expected):
+    assert to_hex(number) == expected
+
+
+@pytest.mark.parametrize(
+    ('message', 'expected'),
+    [
+        ('', 'a2582a3a0e66e6e86e3812dcb672a272'),
+        ('AoC 2017', '33efeb34ea91902bb2f59c9920caa6cd'),
+        ('1,2,3', '3efbe78a8d82f29979031a4aa0b16a9d'),
+        ('1,2,4', '63960835bcdc130f0b66d7ff4f6a5a8e'),
+    ]
+)
+def test_solve(message, expected):
+    assert solve(message) == expected
