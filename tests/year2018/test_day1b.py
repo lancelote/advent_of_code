a54b3205fd274f5d4d68664414806f77b5d8ab9d
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-12-02 16:05:52 +0300

    Fix #136 - 2018 Day 1 Part 2 Solution

---
+++ tests/year2018/test_day1b.py
@@ -0,0 +1,18 @@
+"""2018 - Day 1 Part 2: Chronal Calibration tests."""
+
+import pytest
+
+from src.year2018.day1b import solve
+
+
+@pytest.mark.parametrize(
+    ('changes', 'expected'),
+    [
+        ('+1\n-1', 0),
+        ('+3\n+3\n+4\n-2\n-4', 10),
+        ('-6\n+3\n+8\n+5\n-6', 5),
+        ('+7\n+7\n-2\n-7\n-4', 14),
+    ]
+)
+def test_solve(changes, expected):
+    assert solve(changes) == expected
