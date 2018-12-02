a5c7946ab3b952755ae8d3def1ccfd0d5538fd92
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-12-02 18:09:44 +0300

    Fix #137 - 2018 Day 2 Part 1 Solution

---
+++ tests/year2018/test_day2a.py
@@ -0,0 +1,23 @@
+"""2018 - Day 2 Part 1: Inventory Management System tests."""
+from collections import Counter
+from textwrap import dedent
+
+from src.year2018.day2a import process_data, solve
+
+
+def test_process_data():
+    expected = [Counter({'a': 2, 'b': 1}), Counter({'c': 2})]
+    assert list(process_data('aab\ncc')) == expected
+
+
+def test_solve():
+    task = dedent("""
+        abcdef
+        bababc
+        abbcde
+        abcccd
+        aabcdd
+        abcdee
+        ababab
+    """)
+    assert solve(task) == 12
