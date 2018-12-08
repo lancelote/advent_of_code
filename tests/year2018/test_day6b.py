5a36447e73f4f48de4947fd2de261a4ce5847ca3
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-12-08 18:37:56 +0300

    Fix #146 - 2018 Day 6 Part 2 Solution

---
+++ tests/year2018/test_day6b.py
@@ -0,0 +1,17 @@
+"""2018 - Day 6 Part 2: Chronal Coordinates tests."""
+
+from textwrap import dedent
+
+from src.year2018.day6b import solve
+
+
+def test_solve():
+    sample_task = dedent("""
+        1, 1
+        1, 6
+        8, 3
+        3, 4
+        5, 5
+        8, 9
+    """)
+    assert solve(sample_task, limit=32) == 16
