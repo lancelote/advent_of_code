e78e7f569f551e701f8eeaf028546a1c1488fbe7
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-12-21 09:38:02 +0300

    Rewrite day 11a solution in OOP

--- src/year2018/day11a.py
+++ src/year2018/day11a.py
@@ -80,55 +80,57 @@ from itertools import product
 from typing import DefaultDict, Tuple
 
 
-Grid = DefaultDict[Tuple[int, int], int]
+Power = int
+Cell = Tuple[int, int]
 
 
-def get_square_power(x: int, y: int, grid: Grid) -> int:
-    """Calculate area power level, base is top left corner."""
-    square_power = 0
-    for dx in range(x, x + 3):
-        for dy in range(y, y + 3):
-            square_power += grid[(dx, dy)]
-    return square_power
+class Grid:
+    """Power grid."""
+
+    def __init__(self, serial: int, side: int):
+        """Where serial is grid serial number and side is a side length."""
+        self.side = side
+        self.cells: DefaultDict[Cell, Power] = defaultdict(int)
+        for x, y in product(range(side), range(side)):
+            self.cells[(x, y)] = self.get_power_level(x, y, serial)
+
+    @staticmethod
+    def get_power_level(x: int, y: int, serial: int) -> Power:
+        """Get cell power level."""
+        rack_id = x + 10
+        power_level = rack_id * y
+        power_level += serial
+        power_level *= rack_id
+        try:
+            power_level = int(str(power_level)[-3])
+        except IndexError:
+            power_level = 0
+        power_level -= 5
+        return power_level
+
+    def get_square_power(self, x: int, y: int, size: int) -> Power:
+        """Calculate a cell square sum power."""
+        square_power = 0
+        for dx in range(x, x + size):
+            for dy in range(y, y + size):
+                square_power += self.cells[(dx, dy)]
+        return square_power
+
+    def get_biggest_square_top_left_corner(self, size) -> Cell:
+        """Get left cell coordinates of a most powerful grid square."""
+        biggest_power = -99
+        top_left_corner = (0, 0)
+        side_range = range(self.side - size - 1)
+
+        for x, y in product(side_range, side_range):
+            square_power = self.get_square_power(x, y, size)
+            if square_power > biggest_power:
+                biggest_power = square_power
+                top_left_corner = (x, y)
+        return top_left_corner
 
 
-def get_power_level(x: int, y: int, serial: int) -> int:
-    """Get cell power level."""
-    rack_id = x + 10
-    power_level = rack_id * y
-    power_level += serial
-    power_level *= rack_id
-    try:
-        power_level = int(str(power_level)[-3])
-    except IndexError:
-        power_level = 0
-    power_level -= 5
-    return power_level
-
-
-def get_grid(serial: int, side: int) -> Grid:
-    """Create a grid."""
-    grid: Grid = defaultdict(int)
-    for x, y in product(range(side), range(side)):
-        grid[(x, y)] = get_power_level(x, y, serial)
-    return grid
-
-
-def get_biggest_area(side: int, grid: Grid) -> Tuple[int, int]:
-    """Find the biggest area top left corner."""
-    biggest_area = -99
-    biggest_coord = (0, 0)
-    for x, y in product(range(side - 2), range(side - 2)):
-        area_power = get_square_power(x, y, grid)
-        if area_power > biggest_area:
-            biggest_area = area_power
-            biggest_coord = (x, y)
-    return biggest_coord
-
-
-def solve(task: str) -> Tuple[int, int]:
+def solve(task: str) -> Cell:
     """Find the biggest are top lest corner by a given grid serial."""
-    serial = int(task)
-    side = 300
-    grid = get_grid(serial, side)
-    return get_biggest_area(side, grid)
+    grid = Grid(serial=int(task), side=300)
+    return grid.get_biggest_square_top_left_corner(size=3)
--- tests/year2018/test_day11a.py
+++ tests/year2018/test_day11a.py
@@ -2,29 +2,7 @@
 
 import pytest
 
-from src.year2018.day11a import get_power_level, solve, get_grid,\
-    get_square_power, get_biggest_area
-
-
-@pytest.mark.parametrize(
-    ('x', 'y', 'serial', 'power_level'),
-    [
-        (3, 5, 8, 4),
-        (122, 79, 57, -5),
-        (217, 196, 39, 0),
-        (101, 153, 71, 4),
-    ]
-)
-def test_get_power_level(x, y, serial, power_level):
-    assert get_power_level(x, y, serial) == power_level
-
-
-def test_get_square_power():
-    assert get_square_power(0, 0, get_grid(1, 3)) == -35
-
-
-def test_biggest_area():
-    assert get_biggest_area(3, get_grid(1, 3)) == (0, 0)
+from src.year2018.day11a import solve
 
 
 @pytest.mark.parametrize(
