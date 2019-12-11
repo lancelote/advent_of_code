aeed6c1f050ffe2a272eaea68dab3334296a3f18
Pavel Karateev <lancelote.du.lac@gmail.com>
2019-12-11 14:33:40 +0300

    Fix #210 - 2019 day 4 part 2 solution

---
+++ tests/src/year2019/test_day4b.py
@@ -0,0 +1,14 @@
+"""2019 - Day 4 Part 2: Secure Container tests."""
+
+import pytest
+
+from src.year2019.day4b import at_least_one_equal_pair
+
+
+@pytest.mark.parametrize('num,expected', [
+    ('112233', True),
+    ('123444', False),
+    ('111122', True),
+])
+def test_at_least_one_equal_pair(num, expected):
+    assert at_least_one_equal_pair(num) is expected
