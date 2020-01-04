31c94f1c26399569748154db0aa999c4a3aec3df
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-01-04 18:47:23 +0300

    Fix #215, 2019 day 6 part 2

---
+++ src/year2019/day6b.py
@@ -0,0 +1,44 @@
+"""2019 - Day 6 Part 2: Universal Orbit Map."""
+
+from collections import defaultdict
+from typing import DefaultDict, Set
+
+MAP = DefaultDict[str, Set]
+
+
+def process_data(data: str) -> MAP:
+    """Create a orbit map from raw data."""
+    orbit_map: MAP = defaultdict(set)
+
+    for line in data.strip().split('\n'):
+        obj1, obj2 = line.split(')')
+        orbit_map[obj1].add(obj2)
+        orbit_map[obj2].add(obj1)
+
+    return orbit_map
+
+
+def closest_distance(start: str, stop: str, orbit_map: MAP) -> int:
+    """Find closest distance between start and stop."""
+    distance = 0
+    to_visit_now = {start}
+    to_visit_next = set()
+
+    while to_visit_now:
+        for node in to_visit_now:
+            if node == stop:
+                return distance - 2
+            else:
+                to_visit_next.update(orbit_map[node])
+
+        distance += 1
+        to_visit_now = to_visit_next - to_visit_now
+        to_visit_next = set()
+
+    return -1
+
+
+def solve(task: str) -> int:
+    """Calculate closest distance between YOU and SAN."""
+    orbit_map = process_data(task)
+    return closest_distance('YOU', 'SAN', orbit_map)
---
+++ tests/src/year2019/test_day6b.py
@@ -0,0 +1,26 @@
+"""2019 - Day 6 Part 2: Universal Orbit Map tests."""
+
+import pytest
+
+from src.year2019.day6b import process_data, closest_distance
+
+
+@pytest.fixture
+def raw_orbit_data():
+    return (
+        'COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\n'
+        'G)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN'
+    )
+
+
+def test_process_data(raw_orbit_data):
+    orbit_map = process_data(raw_orbit_data)
+
+    assert len(orbit_map) == 14
+    assert orbit_map['D'] == {'C', 'E', 'I'}
+
+
+def test_closest_distance(raw_orbit_data):
+    orbit_map = process_data(raw_orbit_data)
+
+    assert closest_distance('YOU', 'SAN', orbit_map) == 4
