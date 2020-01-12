fb464b34c2d1b897e4fc7f81fa5d3a0541a62e73
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-01-12 20:46:04 +0300

    Switch from inclination to azimuth

--- src/year2019/day10a.py
+++ src/year2019/day10a.py
@@ -24,17 +24,14 @@ class Chart:
 
     def visible_from(self, base_x: int, base_y: int) -> int:
         """Find the number of visible asteroids from this one."""
-        inclinations = set()
+        azimuth = set()
 
         for y in range(len(self.locations)):
             for x in range(len(self.locations[0])):
                 if self.locations[y][x] and not (x == base_x and y == base_y):
-                    new_x = x - base_x
-                    new_y = y - base_y
-                    tan = new_y / new_x if new_x else math.inf
-                    inclinations.add((new_x > 0, new_y > 0, tan))
+                    azimuth.add(math.atan2(y - base_y, x - base_x))
 
-        return len(inclinations)
+        return len(azimuth)
 
     @property
     def most_observant(self) -> int:
