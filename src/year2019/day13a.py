da42cb238f3d6b464e82c4bbd9101fc93e37799e
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-01-27 15:43:11 +0300

    Fix #228 2019 day 13 part 1

---
+++ src/year2019/day13a.py
@@ -0,0 +1,11 @@
+"""2019 - Day 13 Part 1: Care Package."""
+
+from src.year2019.intcode import Computer
+
+
+def solve(task: str) -> int:
+    """Count the number of blocks."""
+    computer = Computer()
+    computer.load_program(task)
+    computer.execute()
+    return list(computer.stdout)[2::3].count(2)
