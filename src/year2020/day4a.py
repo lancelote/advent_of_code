72149ff159a4c336a765c1d7612ea2ec530f1e43
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-12-04 23:42:17 +0300

    2020 Day 4 Part 2

--- src/year2020/day4a.py
+++ src/year2020/day4a.py
@@ -5,6 +5,7 @@ import re
 from dataclasses import dataclass
 from typing import Dict
 from typing import List
+from typing import Type
 
 MANDATORY_FIELDS = {
     "byr",  # Birth Year
@@ -21,13 +22,13 @@ MANDATORY_FIELDS = {
 class Passport:
     fields: Dict[str, str]
 
-    @staticmethod
-    def from_str(data: str) -> Passport:
+    @classmethod
+    def from_str(cls, data: str) -> Passport:
         fields = {}
         for field in re.split(r"\s", data.strip()):
             key, value = field.split(":")
             fields[key] = value
-        return Passport(fields=fields)
+        return cls(fields=fields)
 
     @property
     def is_valid(self) -> bool:
@@ -37,11 +38,11 @@ class Passport:
         )
 
 
-def process_data(task: str) -> List[Passport]:
-    return [Passport.from_str(block) for block in task.strip().split("\n\n")]
+def process_data(task: str, document: Type[Passport]) -> List[Passport]:
+    return [document.from_str(block) for block in task.strip().split("\n\n")]
 
 
 def solve(task: str) -> int:
     """Count valid passports."""
-    passports = process_data(task)
+    passports = process_data(task, Passport)
     return sum(passport.is_valid for passport in passports)
