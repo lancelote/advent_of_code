43f429ca623731db61df3577ad72de6a8fe40ed9
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-12-08 22:45:27 +0300

    2020 Day 7 Part 1

---
+++ src/year2020/day7a.py
@@ -0,0 +1,31 @@
+"""2020 - Day 7 Part 1: Handy Haversacks."""
+import re
+from collections import defaultdict
+from typing import Dict
+from typing import List
+
+
+def process_data(task: str) -> Dict[str, List[str]]:
+    parents = defaultdict(list)
+
+    for line in task.strip().split("\n"):
+        parent, *children = re.findall(r"\w+\s\w+(?=\sbag)", line)
+        for child in children:
+            parents[child].append(parent)
+
+    return parents
+
+
+def solve(task: str) -> int:
+    """How many differently colored bags can contain shiny gold?"""
+    parents = process_data(task)
+    seen = set()
+    candidates = parents["shiny gold"]
+
+    while candidates:
+        candidate = candidates.pop()
+        if candidate not in seen:
+            seen.add(candidate)
+        candidates.extend(parents[candidate])
+
+    return len(seen)
