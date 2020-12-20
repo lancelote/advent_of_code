78634d7f9654ce819c90a044271ea90749d75ff5
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-12-20 12:36:19 +0300

    2020 Day 8 Part 2

--- src/year2020/day8a.py
+++ src/year2020/day8a.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum
 from typing import List
+from typing import Tuple
 
 
 class Operation(Enum):
@@ -25,47 +26,61 @@ class Operation(Enum):
 
 @dataclass
 class Instruction:
-    operation: Operation
-    argument: int
-    seen: bool = False
+    op: Operation
+    arg: int
 
     @classmethod
     def from_line(cls, line: str) -> Instruction:
         op, arg = line.split()
         return cls(Operation.from_str(op), int(arg))
 
+    def swap(self) -> None:
+        if self.op is Operation.NOP:
+            self.op = Operation.JMP
+        elif self.op is Operation.JMP:
+            self.op = Operation.NOP
+        else:
+            raise ValueError(f"swap attempt on {self.op}")
+
+    @property
+    def is_swapable(self) -> bool:
+        return self.op is Operation.NOP or self.op is Operation.JMP
+
 
 def process_data(task: str) -> List[Instruction]:
     return [Instruction.from_line(line) for line in task.strip().split("\n")]
 
 
-def run(instructions: List[Instruction]) -> int:
+def run(instructions: List[Instruction]) -> Tuple[bool, int]:
     i = 0
     acc = 0
+    seen = set()
+    graceful = True
 
-    while True:
+    while i < len(instructions):
         current = instructions[i]
 
-        if current.seen:
+        if i in seen:
+            graceful = False
             break
         else:
-            current.seen = True
+            seen.add(i)
 
-        if current.operation is Operation.ACC:
-            acc += current.argument
+        if current.op is Operation.ACC:
+            acc += current.arg
             i += 1
-        elif current.operation is Operation.JMP:
-            i += current.argument
-        elif current.operation is Operation.NOP:
+        elif current.op is Operation.JMP:
+            i += current.arg
+        elif current.op is Operation.NOP:
             i += 1
         else:
-            raise ValueError(f"unknown operation {current.operation}")
+            raise ValueError(f"unknown operation {current.op}")
 
-    return acc
+    return graceful, acc
 
 
 def solve(task: str) -> int:
     """What is the value of accumulator?"""
     instructions = process_data(task)
-    acc = run(instructions)
+    _, acc = run(instructions)
     return acc
---
+++ src/year2020/day8b.py
@@ -0,0 +1,24 @@
+"""2020 - Day 8 Part 1: Handheld Halting."""
+from typing import List
+
+from src.year2020.day8a import Instruction
+from src.year2020.day8a import process_data
+from src.year2020.day8a import run
+
+
+def attempt(instructions: List[Instruction]) -> int:
+    for instruction in instructions:
+        if instruction.is_swapable:
+            instruction.swap()
+            graceful, acc = run(instructions)
+            if graceful:
+                return acc
+            instruction.swap()
+
+    raise ValueError("corrupted operation was not found")
+
+
+def solve(task: str) -> int:
+    """What is the value of accumulator after termination?"""
+    instructions = process_data(task)
+    return attempt(instructions)
