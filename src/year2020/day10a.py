0ff05021580cc909941400e6c5c99de2659d15ba
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-12-25 12:37:39 +0300

    2020 Day 10 Part 1 Solution

---
+++ src/year2020/day10a.py
@@ -0,0 +1,26 @@
+"""2020 - Day 10 Part 1: Adapter Array."""
+from collections import defaultdict
+from typing import DefaultDict
+from typing import List
+
+
+def process_data(data: str) -> List[int]:
+    return [int(x) for x in data.strip().split("\n")]
+
+
+def find_differences(adapters: List[int]) -> DefaultDict[int, int]:
+    current = 0
+    differences: DefaultDict[int, int] = defaultdict(int)
+
+    for adapter in sorted(adapters):
+        differences[adapter - current] += 1
+        current = adapter
+
+    return differences
+
+
+def solve(task: str) -> int:
+    """Jolt difference"""
+    adapters = process_data(task)
+    differences = find_differences(adapters)
+    return differences[1] * (differences[3] + 1)
