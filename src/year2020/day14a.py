62c0b20b46a6c512815a4b1caabf8e0d19dd15f3
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-03-28 18:23:34 +0300

    Simplify 2020 day 14a solution
    - Introduce separate Mask and Memory classes
    - Move execution logic into commands

--- src/year2020/day14a.py
+++ src/year2020/day14a.py
@@ -2,13 +2,54 @@
 from __future__ import annotations
 
 import re
+from abc import ABC
+from abc import abstractmethod
 from collections import defaultdict
 from dataclasses import dataclass
 from typing import DefaultDict
 from typing import List
 
 
-class Command:
+class Mask:
+    def __init__(self):
+        self._mask = "X" * 36
+
+    def set(self, mask: str) -> None:
+        self._mask = mask
+
+    def apply(self, value: int) -> int:
+        str_value = bin(value)[2:].rjust(36, "0")
+        result = []
+
+        for bit, mask_bit in zip(str_value, self._mask):
+            if mask_bit == "X":
+                result.append(bit)
+            elif mask_bit == "0":
+                result.append("0")
+            elif mask_bit == "1":
+                result.append("1")
+            else:
+                raise ValueError(f"unexpected mask bit: {mask_bit}")
+
+        return int("".join(result), 2)
+
+
+class Memory:
+    def __init__(self):
+        self._mask = Mask()
+        self._memory: DefaultDict[int, int] = defaultdict(int)
+
+    def set_mask(self, mask: str) -> None:
+        self._mask.set(mask)
+
+    def add(self, address: int, value: int) -> None:
+        self._memory[address] = self._mask.apply(value)
+
+    def sum(self) -> int:
+        return sum(self._memory.values())
+
+
+class Command(ABC):
     @staticmethod
     def from_line(line: str) -> Command:
         if line.startswith("mask"):
@@ -17,6 +58,10 @@ class Command:
             return MemoryCommand.from_line(line)
         else:
             raise ValueError(f"unexpected command line: {line}")
+
+    @abstractmethod
+    def execute(self, memory: Memory) -> None:
+        ...
 
 
 @dataclass
@@ -27,6 +72,9 @@ class MaskCommand(Command):
     def from_line(line: str) -> MaskCommand:
         _, mask = line.split(" = ")
         return MaskCommand(mask)
+
+    def execute(self, memory: Memory) -> None:
+        memory.set_mask(self.mask)
 
 
 @dataclass
@@ -39,6 +87,9 @@ class MemoryCommand(Command):
         address, value = re.findall(r"\d+", line)
         return MemoryCommand(int(address), int(value))
 
+    def execute(self, memory: Memory) -> None:
+        memory.add(self.address, self.value)
+
 
 def process_data(data: str) -> List[Command]:
     return [Command.from_line(line) for line in data.strip().split("\n")]
@@ -47,38 +98,15 @@ def process_data(data: str) -> List[Command]:
 class Program:
     def __init__(self) -> None:
         self.mask = "X" * 36
-        self.memory: DefaultDict[int, int] = defaultdict(int)
+        self.memory = Memory()
 
     def execute(self, commands: List[Command]) -> None:
         for command in commands:
-            if isinstance(command, MaskCommand):
-                self.mask = command.mask
-            elif isinstance(command, MemoryCommand):
-                self.add(command.address, command.value)
-
-    def add(self, address: int, value: int) -> None:
-        value = self.apply_mask(value)
-        self.memory[address] = value
-
-    def apply_mask(self, value: int) -> int:
-        str_value = bin(value)[2:].rjust(36, "0")
-        result = []
-
-        for bit, mask_bit in zip(str_value, self.mask):
-            if mask_bit == "X":
-                result.append(bit)
-            elif mask_bit == "0":
-                result.append("0")
-            elif mask_bit == "1":
-                result.append("1")
-            else:
-                raise ValueError(f"unexpected mask bit: {mask_bit}")
-
-        return int("".join(result), 2)
+            command.execute(self.memory)
 
     @property
     def memory_sum(self) -> int:
-        return sum(self.memory.values())
+        return self.memory.sum()
 
 
 def solve(task: str) -> int:
