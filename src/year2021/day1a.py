171d3ceffed4ff597c1998f3b2ffca4265dd5c03
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-12-01 12:43:28 +0300

    2021 day 1 part 1 - switch to itertools.pairwise()

--- src/year2021/day1a.py
+++ src/year2021/day1a.py
@@ -1,10 +1,7 @@
 """2021 - Day 1 Part 1: Sonar Sweep."""
+from itertools import pairwise
 
 
 def solve(task: str) -> int:
-    result = 0
-    depths = [int(x) for x in task.strip().split("\n")]
-    for i in range(1, len(depths)):
-        if depths[i] > depths[i - 1]:
-            result += 1
-    return result
+    depths = (int(x) for x in task.strip().split("\n"))
+    return sum(a < b for (a, b) in pairwise(depths))
