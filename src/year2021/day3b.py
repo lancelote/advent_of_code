ad16a0dcbf9fc6714211c9eb9b270efe82ce3acd
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-12-03 22:47:38 +0300

    Rename i_bit to bit_index

--- src/year2021/day3b.py
+++ src/year2021/day3b.py
@@ -8,7 +8,7 @@ Criteria = Callable[[str, int, Iterator[str]], bool]
 
 
 def search(nums: list[str], criteria: Criteria) -> int:
-    i_bit = 0
+    bit_index = 0
     left_indexes = set(range(len(nums)))
 
     while len(left_indexes) != 1:
@@ -16,24 +16,24 @@ def search(nums: list[str], criteria: Criteria) -> int:
 
         for i in left_indexes:
             left_nums = (nums[i] for i in left_indexes)
-            if not criteria(nums[i], i_bit, left_nums):
+            if not criteria(nums[i], bit_index, left_nums):
                 filtered_out.add(i)
 
         left_indexes.difference_update(filtered_out)
-        i_bit += 1
+        bit_index += 1
 
     last_index_left = left_indexes.pop()
     return int(nums[last_index_left], base=2)
 
 
-def oxygen_criteria(num: str, i_bit: int, nums: Iterator[str]) -> bool:
-    i_bits = (num[i_bit] for num in nums)
-    return num[i_bit] == most_common(i_bits)
+def oxygen_criteria(num: str, bit_index: int, nums: Iterator[str]) -> bool:
+    i_bits = (num[bit_index] for num in nums)
+    return num[bit_index] == most_common(i_bits)
 
 
-def co2_criteria(num: str, i_bit: int, nums: Iterator[str]) -> bool:
-    i_bits = (num[i_bit] for num in nums)
-    return num[i_bit] == least_common(i_bits)
+def co2_criteria(num: str, bit_index: int, nums: Iterator[str]) -> bool:
+    i_bits = (num[bit_index] for num in nums)
+    return num[bit_index] == least_common(i_bits)
 
 
 def solve(task: str) -> int:
