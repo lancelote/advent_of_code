ad128609fcc6cd5c5816dd1a89604a62f48bf3a7
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-12-05 20:25:29 +0300

    Simplify 2021 day 4 solution
    No need to store every played num inside every board

--- src/year2021/day4a.py
+++ src/year2021/day4a.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
-from itertools import chain
-from typing import Iterator
-
 
 class Board:
     def __init__(self, nums: list[list[int]]) -> None:
         self.last_draw = 0
         self.nums = nums
-        self.marked: set[int] = set()
+        self.unmarked: set[int] = {x for row in nums for x in row}
 
     @classmethod
     def from_text(cls, text: str) -> Board:
@@ -22,17 +19,17 @@ class Board:
 
     def draw(self, num: int) -> None:
         self.last_draw = num
-        self.marked.add(num)
+        self.unmarked.discard(num)
 
     def has_marked_rows(self) -> bool:
         for row in self.nums:
-            if all(num in self.marked for num in row):
+            if not any(num in self.unmarked for num in row):
                 return True
         return False
 
     def has_marked_cols(self) -> bool:
         for i in range(len(self.nums[0])):
-            if all(row[i] in self.marked for row in self.nums):
+            if not any(row[i] in self.unmarked for row in self.nums):
                 return True
         return False
 
@@ -41,14 +38,8 @@ class Board:
         return self.has_marked_cols() or self.has_marked_rows()
 
     @property
-    def unmarked_nums(self) -> Iterator[int]:
-        for num in chain.from_iterable(self.nums):
-            if num not in self.marked:
-                yield num
-
-    @property
     def score(self) -> int:
-        return self.last_draw * sum(self.unmarked_nums)
+        return self.last_draw * sum(self.unmarked)
 
 
 def solve(task: str) -> int:
