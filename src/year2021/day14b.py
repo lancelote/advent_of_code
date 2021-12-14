bc2ace7459936558db0d67903aaeec0b9439d460
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-12-14 20:25:51 +0300

    2021 day 14 part 2 solution

---
+++ src/year2021/day14b.py
@@ -0,0 +1,52 @@
+"""2021 - Day 14 Part 2: Extended Polymerization."""
+from collections import defaultdict
+from itertools import pairwise
+from typing import Tuple
+
+Pair = Tuple[str, ...]
+Pairs = dict[Pair, int]
+Insertions = dict[Pair, str]
+Counts = dict[str, int]
+
+
+def parse_task(task: str) -> tuple[Pairs, Insertions, Counts]:
+    raw_polymer, raw_insertions = task.split("\n\n")
+
+    counts: Counts = defaultdict(int)
+
+    for element in raw_polymer:
+        counts[element] += 1
+
+    pairs: Pairs = defaultdict(int)
+
+    for a, b in pairwise(raw_polymer):
+        pairs[(a, b)] += 1
+
+    insertions: Insertions = {}
+
+    for insertion in raw_insertions.splitlines():
+        base, output = insertion.split(" -> ")
+        insertions[tuple(base)] = output
+
+    return pairs, insertions, counts
+
+
+def step(pairs: Pairs, insertions: Insertions, counts: Counts) -> Pairs:
+    new_pairs: dict[Pair, int] = defaultdict(int)
+
+    for (a, b), count in pairs.items():
+        c = insertions[(a, b)]
+        new_pairs[(a, c)] += count
+        new_pairs[(c, b)] += count
+        counts[c] += count
+
+    return new_pairs
+
+
+def solve(task: str) -> int:
+    pairs, insertions, counts = parse_task(task)
+
+    for _ in range(40):
+        pairs = step(pairs, insertions, counts)
+
+    return max(counts.values()) - min(counts.values())
---
+++ tests/src/year2021/test_day14b.py
@@ -0,0 +1,30 @@
+"""2021 - Day 14 Part 2: Extended Polymerization."""
+from textwrap import dedent
+
+from src.year2021.day14b import solve
+
+
+def test_solve():
+    task = dedent(
+        """
+        NNCB
+
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
+        """
+    ).strip()
+    assert solve(task) == 2188189693529
