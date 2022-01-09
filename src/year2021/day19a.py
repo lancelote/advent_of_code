504ee67c6dee809a9c131ea20a8735d9a693f04c
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-01-09 13:58:55 +0300

    `Scanner.from_text()` implementation

--- src/year2021/day19a.py
+++ src/year2021/day19a.py
@@ -1,6 +1,7 @@
 """2021 - Day 19 Part 1: Beacon Scanner."""
 from __future__ import annotations
 
+import re
 from dataclasses import dataclass
 from typing import NamedTuple
 
@@ -24,6 +25,14 @@ class Scanner:
     pk: int
     beacons: list[Position]
 
+    @classmethod
+    def from_text(cls, text: str) -> Scanner:
+        [header, *body] = text.splitlines()
+        [pk_str] = re.findall(r"\d+", header)
+        pk = int(pk_str)
+        beacons = [Position.from_line(line) for line in body]
+        return Scanner(pk, beacons)
+
 
 def solve(task: str) -> int:
-    ...
+    scanners = [Scanner.from_text(text) for text in task.split("\n\n")]
--- tests/src/year2021/test_day19a.py
+++ tests/src/year2021/test_day19a.py
@@ -1,7 +1,10 @@
 """2021 - Day 19 Part 1: Beacon Scanner."""
+from textwrap import dedent
+
 import pytest
 
 from src.year2021.day19a import Position
+from src.year2021.day19a import Scanner
 
 
 @pytest.mark.parametrize(
@@ -14,3 +17,19 @@ from src.year2021.day19a import Position
 def test_position_from_line(line, position):
     assert Position.from_line(line) == position
 
+
+def test_scanner_from_text():
+    text = dedent("""
+        --- scanner 0 ---
+        645,-448,-766
+        529,751,-867
+    """).strip()
+    actual_scanner = Scanner.from_text(text)
+    expected_scanner = Scanner(
+        pk=0,
+        beacons=[
+            Position(645, -448, -766),
+            Position(529, 751, -867),
+        ]
+    )
+    assert actual_scanner == expected_scanner
