a4149415d310ea9ea435904375bcd9b209250396
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-12-02 19:05:10 +0100

    Update 2015 day 3

--- src/year2015/day03a.py
+++ src/year2015/day03a.py
@@ -1,48 +1,29 @@
 """2015 - Day 3 Part 1: Perfectly Spherical Houses in a Vacuum."""
 from collections import defaultdict
-from collections import namedtuple
 
-Coordinates = namedtuple("Coordinates", ["x", "y"])
+
 SHIFT = {
-    "<": Coordinates(-1, 0),
-    "^": Coordinates(0, 1),
-    ">": Coordinates(1, 0),
-    "v": Coordinates(0, -1),
+    "<": (-1, 0),
+    "^": (0, 1),
+    ">": (1, 0),
+    "v": (0, -1),
 }
 
 
-def visit_houses(task: str) -> dict[Coordinates, int]:
-    """Deliver presents.
-
-    Args:
-        task (str): '>^<v...'
-
-    Returns:
-        dct: {position: number_of_presents, ...}
-
-    """
-    current_position = Coordinates(0, 0)
+def visit_houses(task: str) -> dict[tuple[int, int], int]:
+    x, y = 0, 0
     visited_houses = defaultdict(int)
-    visited_houses[current_position] = 1
+    visited_houses[(0, 0)] = 1
 
     for direction in task:
-        current_position = Coordinates(
-            current_position.x - SHIFT[direction].x,
-            current_position.y - SHIFT[direction].y,
-        )
-        visited_houses[current_position] += 1
+        dx, dy = SHIFT[direction]
+        x -= dx
+        y -= dy
+        visited_houses[(x, y)] += 1
+
     return visited_houses
 
 
 def solve(task: str) -> int:
-    """Solve the puzzle.
-
-    Args:
-        task (str): '>^<v...'
-
-    Returns:
-        int: Number of houses with at least one present
-
-    """
     visited_houses = visit_houses(task)
     return len(visited_houses)
--- src/year2015/day03b.py
+++ src/year2015/day03b.py
@@ -3,15 +3,6 @@ from src.year2015.day03a import visit_houses
 
 
 def solve(task: str) -> int:
-    """Solve the puzzle.
-
-    Args:
-        task (str): '>^<v...'
-
-    Returns:
-        int: Number of houses with at least one present
-
-    """
     santa_task = task[0::2]
     robo_santa_task = task[1::2]
     visited_by_santa = visit_houses(santa_task)
