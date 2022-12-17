fc588b48eb61caac41c6b0a5b39054666c900742
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-12-17 12:04:44 +0100

    2022 day 13 part 1

---
+++ src/year2022/day13a.py
@@ -0,0 +1,64 @@
+"""2022 - Day 13 Part 1: Distress Signal."""
+from __future__ import annotations
+
+import ast
+from typing import TypeAlias
+from typing import Union
+
+Packet: TypeAlias = list[Union[int, "Packet"]]
+Pair: TypeAlias = tuple[Packet, Packet]
+
+
+def parse_packet(line: str) -> Packet:
+    packet: Packet = ast.literal_eval(line)
+    return packet
+
+
+def compare(packet1: Packet | int, packet2: Packet | int) -> int:
+    lst1: Packet
+    lst2: Packet
+
+    if isinstance(packet1, int) and isinstance(packet2, int):
+        return packet1 - packet2
+
+    if isinstance(packet1, int):
+        lst1 = [packet1]
+    else:
+        lst1 = packet1
+
+    if isinstance(packet2, int):
+        lst2 = [packet2]
+    else:
+        lst2 = packet2
+
+    for x1, x2 in zip(lst1, lst2):
+        item_result = compare(x1, x2)
+        if item_result != 0:
+            return item_result
+
+    return len(lst1) - len(lst2)
+
+
+def parse_pairs(task: str) -> list[Pair]:
+    result: list[Pair] = []
+
+    for pair_str in task.split("\n\n"):
+        packet1_line, packet2_line = pair_str.splitlines()
+
+        packet1 = parse_packet(packet1_line)
+        packet2 = parse_packet(packet2_line)
+
+        result.append((packet1, packet2))
+
+    return result
+
+
+def solve(task: str) -> int:
+    indices: list[int] = []
+
+    for i, pair in enumerate(parse_pairs(task), start=1):
+        packet1, packet2 = pair
+        if compare(packet1, packet2) <= 0:
+            indices.append(i)
+
+    return sum(indices)
---
+++ tests/src/year2022/test_day13a.py
@@ -0,0 +1,59 @@
+"""2022 - Day 13 Part 1: Distress Signal."""
+from textwrap import dedent
+
+import pytest
+
+from src.year2022.day13a import compare
+from src.year2022.day13a import solve
+
+
+@pytest.mark.parametrize(
+    "packet1,packet2,right",
+    [
+        ([1, 1, 3, 1, 1], [1, 1, 5, 1, 1], True),
+        ([[1], [2, 3, 4]], [[1], 4], True),
+        ([9], [[8, 7, 6]], False),
+        ([[4, 4], 4, 4], [[4, 4], 4, 4, 4], True),
+        ([7, 7, 7, 7], [7, 7, 7], False),
+        ([], [3], True),
+        ([[[]]], [[]], False),
+        (
+            [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+            [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+            False,
+        ),
+    ],
+)
+def test_compare(packet1, packet2, right):
+    assert (compare(packet1, packet2) <= 0) is right
+
+
+def test_solve():
+    task = dedent(
+        """
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+
+        [[1],[2,3,4]]
+        [[1],4]
+
+        [9]
+        [[8,7,6]]
+
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+
+        [7,7,7,7]
+        [7,7,7]
+
+        []
+        [3]
+
+        [[[]]]
+        [[]]
+
+        [1,[2,[3,[4,[5,6,7]]]],8,9]
+        [1,[2,[3,[4,[5,6,0]]]],8,9]
+        """
+    ).strip()
+    assert solve(task) == 13
