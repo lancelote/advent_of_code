1160a453a795429e78446da7f0b56a0a04a5f82b
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-12-29 16:44:06 +0100

    2022 day 16 part 1 naive working solution

--- src/year2022/day16a.py
+++ src/year2022/day16a.py
@@ -23,26 +23,46 @@ def parse_flow_rates(task: str) -> dict[str, int]:
     return flow_rates
 
 
+def left_score(
+    flow_rate: dict[str, int], released_valves: frozenset[str], minute: int = 0
+) -> int:
+    total_score = 0
+
+    for valve, score in flow_rate.items():
+        if valve not in released_valves:
+            total_score += score * minute
+
+    return total_score
+
+
 def solve(task: str) -> int:
     flow_rate = parse_flow_rates(task)
     tunnels = parse_tunnels(task)
-    to_visit = {("AA", 0, frozenset(("AA",)))}
+    to_visit = {("AA", 0, frozenset(("AA",)), frozenset(("AA",)))}
+    max_score = 0
 
     for minute in range(29, -1, -1):
         new_to_visit = set()
 
-        for valve, score, released in to_visit:
-            if flow_rate[valve] and valve not in released:
-                new_to_visit.add(
-                    (
-                        valve,
-                        score + minute * flow_rate[valve],
-                        released | {valve},
-                    )
-                )
+        for valve, score, released, visited in to_visit:
+            if left_score(flow_rate, released, minute) + score < max_score:
+                continue
+
+            max_score = max(max_score, score)
+
             for neighbor in tunnels[valve]:
-                new_to_visit.add((neighbor, score, released))
+                if neighbor not in visited:
+                    new_to_visit.add(
+                        (neighbor, score, released, visited | {neighbor})
+                    )
+
+            if flow_rate[valve] and valve not in released:
+                visited = frozenset((valve,))
+                released |= {valve}
+                score += minute * flow_rate[valve]
+
+                new_to_visit.add((valve, score, released, visited))
 
         to_visit = new_to_visit
 
-    return max(score for _, score, _ in to_visit)
+    return max_score
