ac42239b0e225d1a58f6a2c4cd86744c518e4f05
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-12-01 21:24:50 +0100

    Simplify conversion logic

--- src/year2023/day01b.py
+++ src/year2023/day01b.py
@@ -4,15 +4,15 @@ import re
 from src.year2023.day01a import process_data
 
 TO_DIGIT = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
 }
 
 
@@ -20,20 +20,13 @@ def get_num(line: str) -> int:
     pattern = r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"
     match = re.findall(pattern, line)
 
-    first = match[0]
+    first: str = match[0]
     last = match[-1]
 
-    try:
-        first_int = int(first)
-    except ValueError:
-        first_int = TO_DIGIT[first]
+    first = first if first.isdigit() else TO_DIGIT[first]
+    last = last if last.isdigit() else TO_DIGIT[last]
 
-    try:
-        last_int = int(last)
-    except ValueError:
-        last_int = TO_DIGIT[last]
-
-    return int(str(first_int) + str(last_int))
+    return int(first + last)
 
 
 def solve(task: str) -> int:
