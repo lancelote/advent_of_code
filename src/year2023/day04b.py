56d7cd9a871d4afea11fd99060c50affa81df83a
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-12-04 11:50:02 +0100

    2023 day 4 part 2

---
+++ src/year2023/day04b.py
@@ -0,0 +1,18 @@
+"""2023 - Day 4 Part 2: Scratchcards"""
+from collections import defaultdict
+
+from src.year2023.day04a import process_data
+
+
+def solve(task: str) -> int:
+    count = 0
+    cards = process_data(task)
+    copies: dict[int, int] = defaultdict(lambda: 1)
+
+    for i, card in enumerate(cards):
+        count += copies[i]
+        for j in range(1, card.matching + 1):
+            if i + j < len(cards):
+                copies[i + j] += copies[i]
+
+    return count
