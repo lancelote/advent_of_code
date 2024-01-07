6145f6b9eeee21977ead96cbe66bf8723e61cb6a
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-01-07 19:45:12 +0100

    2023 day 15 part 2

---
+++ src/year2023/day15b.py
@@ -0,0 +1,41 @@
+"""2023 - Day 15 Part 2: Lens Library"""
+import re
+from collections import defaultdict
+from typing import TypeAlias
+
+from src.year2023.day15a import get_hash
+
+Boxes: TypeAlias = dict[int, dict[str, int]]
+
+
+def parse_task(task: str) -> Boxes:
+    boxes: Boxes = defaultdict(dict)
+
+    for step in task.split(","):
+        [(label, op, focal)] = re.findall(r"([a-z]+)([=\-])(\d+)?", step)
+        box = get_hash(label)
+
+        if op == "=":
+            boxes[box][label] = int(focal)
+        elif op == "-":
+            if label in boxes[box]:
+                del boxes[box][label]
+        else:
+            raise ValueError(f"unknown op: {op}")
+
+    return boxes
+
+
+def get_focus_power(boxes: Boxes) -> int:
+    total = 0
+
+    for box in range(256):
+        for slot, (_, focal) in enumerate(boxes[box].items(), start=1):
+            total += (box + 1) * slot * focal
+
+    return total
+
+
+def solve(task: str) -> int:
+    boxes = parse_task(task)
+    return get_focus_power(boxes)
---
+++ tests/src/year2023/test_day15b.py
@@ -0,0 +1,13 @@
+"""2023 - Day 15 Part 2: Lens Library"""
+from textwrap import dedent
+
+from src.year2023.day15b import solve
+
+
+def test_solve():
+    task = dedent(
+        """
+        rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+        """
+    ).strip()
+    assert solve(task) == 145
