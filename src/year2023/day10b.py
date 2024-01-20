e922fc0bb9d12f6ab35b1dd779f71e7213c09aee
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-01-20 23:37:14 +0100

    2023 day 10 part 2

---
+++ src/year2023/day10b.py
@@ -0,0 +1,85 @@
+"""2023 - Day 10 Part 2: Pipe Maze"""
+from src.year2023.day10a import C
+from src.year2023.day10a import construct_map
+from src.year2023.day10a import find_start
+from src.year2023.day10a import Land
+from src.year2023.day10a import Map
+from src.year2023.day10a import process_data
+
+
+def get_path(m: Map, land: Land) -> list[C]:
+    start = find_start(land)
+
+    def construct_path_from(prev: C, curr: C) -> list[C]:
+        path: list[C] = []
+
+        while land[curr[0]][curr[1]] != "S":
+            nxt_set = m[curr] - {prev}
+
+            if not nxt_set:
+                return []
+
+            path.append(curr)
+            prev = curr
+            (curr,) = nxt_set
+
+        path.append(start)
+        return path
+
+    for x in m[start]:
+        if p := construct_path_from(start, x):
+            return p
+
+    raise ValueError("path not found")
+
+
+def get_corners(path: list[C], land: Land) -> list[C]:
+    corners: list[C] = []
+
+    for i in range(len(path) - 1):
+        r, c = path[i]
+
+        if land[r][c] in {"L", "J", "7", "F"}:
+            corners.append(path[i])
+
+    def is_start_corner() -> bool:
+        pr, pc = path[0]
+        nr, nc = path[-2]
+
+        p = land[pr][pc]
+        n = land[nr][nc]
+
+        return not (p == n and p in {"|", "-"})
+
+    if is_start_corner():
+        corners.append(path[-1])
+
+    return corners
+
+
+def get_gauss_area(corners: list[C]) -> int:
+    n = len(corners)
+
+    return (
+        abs(
+            sum(
+                corners[i][0] * (corners[i - 1][1] - corners[(i + 1) % n][1])
+                for i in range(n)
+            )
+        )
+        // 2
+    )
+
+
+def solve(task: str) -> int:
+    land = process_data(task)
+    m = construct_map(land)
+    path = get_path(m, land)
+    corners = get_corners(path, land)
+
+    # geometry goes brrr
+    area = get_gauss_area(corners)
+    boundary_points = len(path)
+    inner_points = area - boundary_points // 2 + 1
+
+    return inner_points
---
+++ tests/src/year2023/test_day10b.py
@@ -0,0 +1,100 @@
+"""2023 - Day 10 Part 2: Pipe Maze"""
+from textwrap import dedent
+
+from src.year2023.day10b import solve
+
+
+def test_solve_0():
+    task = dedent(
+        """
+        .....
+        .S-7.
+        .|.|.
+        .L-J.
+        .....
+        """
+    ).strip()
+    assert solve(task) == 1
+
+
+def test_solve_1():
+    task = dedent(
+        """
+        ..F7.
+        .FJ|.
+        SJ.L7
+        |F--J
+        LJ...
+        """
+    ).strip()
+    assert solve(task) == 1
+
+
+def test_solve_2():
+    task = dedent(
+        """
+        ...........
+        .S-------7.
+        .|F-----7|.
+        .||.....||.
+        .||.....||.
+        .|L-7.F-J|.
+        .|..|.|..|.
+        .L--J.L--J.
+        ...........
+        """
+    ).strip()
+    assert solve(task) == 4
+
+
+def test_solve_3():
+    task = dedent(
+        """
+        ..........
+        .S------7.
+        .|F----7|.
+        .||....||.
+        .||....||.
+        .|L-7F-J|.
+        .|..||..|.
+        .L--JL--J.
+        ..........
+        """
+    ).strip()
+    assert solve(task) == 4
+
+
+def test_solve_4():
+    task = dedent(
+        """
+        .F----7F7F7F7F-7....
+        .|F--7||||||||FJ....
+        .||.FJ||||||||L7....
+        FJL7L7LJLJ||LJ.L-7..
+        L--J.L7...LJS7F-7L7.
+        ....F-J..F7FJ|L7L7L7
+        ....L7.F7||L7|.L7L7|
+        .....|FJLJ|FJ|F7|.LJ
+        ....FJL-7.||.||||...
+        ....L---J.LJ.LJLJ...
+        """
+    ).strip()
+    assert solve(task) == 8
+
+
+def test_solve_5():
+    task = dedent(
+        """
+        FF7FSF7F7F7F7F7F---7
+        L|LJ||||||||||||F--J
+        FL-7LJLJ||||||LJL-77
+        F--JF--7||LJLJ7F7FJ-
+        L---JF-JLJ.||-FJLJJ7
+        |F|F-JF---7F7-L7L|7|
+        |FFJF7L7F-JF7|JL---7
+        7-L-JL7||F7|L7F-7F7|
+        L.L7LFJ|||||FJL7||LJ
+        L7JLJL-JLJLJL--JLJ.L
+        """
+    ).strip()
+    assert solve(task) == 10
