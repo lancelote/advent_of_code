f8af151161877ea2e57553236f45db6d127d4324
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-12-01 18:20:31 +0100

    re-solve 2015 day 1 part 2

--- tests/src/year2015/test_day01b.py
+++ tests/src/year2015/test_day01b.py
@@ -1,11 +1,16 @@
 """2015 - Day 1 Part 2: Not Quite Lisp."""
 
-import unittest
+import pytest
 
 from src.year2015.day01b import solve
 
 
-class TestSolve(unittest.TestCase):
-    def test_returns_correct_result(self):
-        self.assertEqual(solve(")"), 1)
-        self.assertEqual(solve("()())"), 5)
+@pytest.mark.parametrize(
+    "task,expected",
+    (
+        (")", 1),
+        ("()())", 5),
+    ),
+)
+def test_returns_correct_result(task, expected):
+    assert solve(task) == expected
