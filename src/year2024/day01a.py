68a3b5f1261a90fc960794f0764c98e7a35b9236
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-12-01 16:52:37 +0100

    2024 day 1 part 1

---
+++ src/year2024/day01a.py
@@ -0,0 +1,16 @@
+"""2024 - Day 1 Part 1:"""
+
+
+def solve(task: str) -> int:
+    left: list[int] = []
+    right: list[int] = []
+
+    for line in task.split("\n"):
+        a, b = line.split()
+        left.append(int(a))
+        right.append(int(b))
+
+    left.sort()
+    right.sort()
+
+    return sum(abs(x - y) for x, y in zip(left, right))
