1f1aa9f6527a1a2f22cde193fd66ff57057fc5f1
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-12-22 19:34:41 +0100

    2024 day 14 part 2 testable solution

--- src/year2024/day14b.py
+++ src/year2024/day14b.py
@@ -1,8 +1,14 @@
 """2024 - Day 14 Part 2: Restroom Redoubt"""
 
+import sys
 from dataclasses import dataclass
+from itertools import combinations
 
 from src.year2024.day14a import Robot
+
+
+def distance(r1: Robot, r2: Robot) -> int:
+    return abs(r1.x - r2.x) + abs(r1.y - r2.y)
 
 
 @dataclass
@@ -16,6 +22,10 @@ class Field:
             robot.x = (robot.x + robot.dx) % self.width
             robot.y = (robot.y + robot.dy) % self.height
 
+    @property
+    def entropy(self) -> int:
+        return sum(distance(a, b) for a, b in combinations(self.robots, 2))
+
     def __str__(self) -> str:
         data = [[" " for _ in range(self.width)] for _ in range(self.height)]
         for robot in self.robots:
@@ -27,9 +37,15 @@ def solve(task: str, width: int = 101, height: int = 103) -> int:
     robots = [Robot.from_line(x) for x in task.split("\n")]
     field = Field(width, height, robots)
 
+    min_entropy_second = 0
+    min_entropy_seen = sys.maxsize
+
     for i in range(1, 10_000):
         field.make_step()
-        print("second", i)
-        print(field)
+        field_entropy = field.entropy
 
-    return -1
+        if field_entropy < min_entropy_seen:
+            min_entropy_seen = field_entropy
+            min_entropy_second = i
+
+    return min_entropy_second
---
+++ tests/src/year2024/test_day14b.py
@@ -0,0 +1,25 @@
+"""2024 - Day 14 Part 2: Restroom Redoubt"""
+
+from textwrap import dedent
+
+from src.year2024.day14b import solve
+
+
+def test_solve():
+    task = dedent(
+        """
+        p=0,4 v=3,-3
+        p=6,3 v=-1,-3
+        p=10,3 v=-1,2
+        p=2,0 v=2,-1
+        p=0,0 v=1,3
+        p=3,0 v=-2,-2
+        p=7,6 v=-1,-3
+        p=3,0 v=-1,-2
+        p=9,3 v=2,3
+        p=7,3 v=-1,2
+        p=2,4 v=2,-3
+        p=9,5 v=-3,-3
+        """
+    ).strip()
+    assert solve(task, width=11, height=7) == 31
