319e57ae3d704e7505a24dea0c97a728382c955f
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-12-07 19:46:28 +0100

    simplify 2025 day 7 part 2

--- src/year2025/day07b.py
+++ src/year2025/day07b.py
@@ -9,15 +9,13 @@ def solve(task: str) -> int:
     n_cols = len(manifold[0])
 
     start_beam = manifold[0].index("S")
-    beams = {start_beam}
     timelines: dict[int, int] = defaultdict(int)
     timelines[start_beam] = 1
 
     for row in manifold:
-        new_beams: set[int] = set()
         new_timelines: dict[int, int] = defaultdict(int)
 
-        for beam in beams:
+        for beam, timeline in timelines.items():
             timeline = timelines[beam]
 
             if row[beam] == "^":
@@ -25,17 +23,13 @@ def solve(task: str) -> int:
                 right = beam + 1
 
                 if 0 <= left < n_cols:
-                    new_beams.add(left)
                     new_timelines[left] += timeline
 
                 if 0 <= right < n_cols:
-                    new_beams.add(right)
                     new_timelines[right] += timeline
             else:
-                new_beams.add(beam)
                 new_timelines[beam] += timeline
 
-        beams = new_beams
         timelines = new_timelines
 
     return sum(timelines.values())
