457b87499414e69cb41741d3a2786e8c35547c85
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-01-15 16:09:13 +0100

    fix `main` tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,25 +10,25 @@ def test_solve(mock_get_data, capsys):
     data = "411 players; last marble is worth 71058 points"
     mock_get_data.return_value = data
 
-    main("2018", "9", "a")
+    main(["2018", "9", "a"])
 
     assert capsys.readouterr().out == "answer: 424639\n"
 
 
 def test_wrong_year():
     with pytest.raises(ValueError, match="unsupported year: 1812"):
-        main("1812", "5", "a")
+        main(["1812", "5", "a"])
 
 
 @pytest.mark.parametrize("day", ["0", "32"])
 def test_wrong_day(day):
     with pytest.raises(ValueError, match=f"want int in range 1-31, got {day}"):
-        main("2017", day, "a")
+        main(["2017", day, "a"])
 
 
 def test_wrong_part():
     with pytest.raises(ValueError, match="want either `a` or `b`, got `c`"):
-        main("2017", "5", "c")
+        main(["2017", "5", "c"])
 
 
 def test_correct_solution_file_names():
